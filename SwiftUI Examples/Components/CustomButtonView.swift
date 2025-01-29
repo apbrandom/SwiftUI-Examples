9d77df36942444d9a4067f1c2539184ab9123292
apbrandom <apbrandom@gmail.com>
2025-01-29 21:14:47 +0100

    CustomButtonView() Design

---
+++ SwiftUI Examples/Components/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  SwiftUI Examples
+//
+//  Created by Vadim Vinogradov on 1/29/25.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
